e8ca92ee677ba69b7018bd8b9e8d7420a20f6f1c
Dylan_Fritz <dfritz1211@gmail.com>
2023-04-04 13:55:26 -0500

    Dynamic username

--- code/id-upload.py
+++ code/id-upload.py
@@ -17,7 +17,7 @@ FILE_PATH = None
 
 try:
     conn = mariadb.connect(
-        user="W11-TOPPLAB23",
+        user=os.getlogin(),
         password=getpass.getpass(prompt='Database user password: '),
         host="10.16.0.101", #Nebula's relational ip!
         port=3306,
